0f90eb2ba45ff5dfaefde38c0bfed5500099bed0
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-03 19:29:24 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,4 +8,13 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
+  
+  hash = {
+    :chipmunks => ["Alvin", "Simon", "Theodore"],
+    :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
+    :jetsons => ["George", "Jane", "Judy", "Elroy"]
+  }
+
+return  hash[show].push(name)
+
 end
